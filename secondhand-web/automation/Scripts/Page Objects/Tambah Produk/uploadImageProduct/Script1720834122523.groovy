0e00a3e0283baca54869392e09e8e75290d61725
werved <vedityawery@gmail.com>
2024-07-18 15:55:02 +0700

    resolving file path for upload images

--- secondhand-web/automation/Scripts/Page Objects/Tambah Produk/uploadImageProduct/Script1720834122523.groovy
+++ secondhand-web/automation/Scripts/Page Objects/Tambah Produk/uploadImageProduct/Script1720834122523.groovy
@@ -16,6 +16,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.configuration.RunConfiguration
+
 
 WebUI.uploadFile(findTestObject('Pages/Tambah Produk/imageUploader'), path)
 
