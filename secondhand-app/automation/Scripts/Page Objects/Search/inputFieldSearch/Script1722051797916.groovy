55a322867bae1e65c60ea410f1700ffb99d8ac09
Lau <laurensia@ymail.com>
2024-07-27 13:21:58 +0700

    add method action to sendKeys from keyboard

--- secondhand-app/automation/Scripts/Page Objects/Search/inputFieldSearch/Script1722051797916.groovy
+++ secondhand-app/automation/Scripts/Page Objects/Search/inputFieldSearch/Script1722051797916.groovy
@@ -7,6 +7,7 @@ import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
@@ -16,8 +17,16 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.interactions.Actions
+
+driver = MobileDriverFactory.getDriver()
 
 Mobile.tap(findTestObject('Pages/Page_Search/Search'), 3, FailureHandling.STOP_ON_FAILURE)
 
-Mobile.setText(findTestObject('Pages/Page_Search/Search'), expected, 0)
+Mobile.waitForElementPresent(findTestObject('Pages/Page_Search/BtnBack'), 5)
+
+Actions keyboard = new Actions(driver)
+keyboard.sendKeys(expected).perform()
+
+Mobile.checkElement(findTestObject(null), 0)
 
