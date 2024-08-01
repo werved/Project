44414d090fc66b8ef55e77119d87a0a07170aed7
faaiqa <abdillahfaaiq@gmail.com>
2024-08-01 13:20:13 +0700

    add and modified automation telusuri kategori, add page object txt banner

--- secondhand-web/automation/Object Repository/Pages/Homepage/btnKategoriElektronik.rs
+++ secondhand-web/automation/Object Repository/Pages/Homepage/btnKategoriElektronik.rs
@@ -6,37 +6,43 @@
    <elementGuidId>c4ebc666-ee41-48bf-bf65-40aaf6bc0b35</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>CSS</key>
+      </entry>
+      <entry>
          <key>BASIC</key>
-         <value>//a[(text() = 'Elektronik
-' or . = 'Elektronik
-')]</value>
+         <value>//*[@href = '/?category_id=4']</value>
       </entry>
       <entry>
          <key>XPATH</key>
          <value>(.//*[normalize-space(text()) and normalize-space(.)='Kaligrafi'])[1]/preceding::a[2]</value>
       </entry>
-      <entry>
-         <key>CSS</key>
-      </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
       <value>a</value>
-      <webElementGuid>3ebcb003-44fb-4e63-ae04-0c3818e1ad2f</webElementGuid>
+      <webElementGuid>6e83a05d-7d1f-4b01-854f-d1b0f67379d6</webElementGuid>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
       <value>Elektronik
 </value>
-      <webElementGuid>4ba1e4bd-e129-4f48-802d-7c9d7c368038</webElementGuid>
+      <webElementGuid>12e93264-d849-4a01-bb44-c7522c5e2d8d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/?category_id=4</value>
+      <webElementGuid>ea876492-2494-4f2a-9021-8d7205dd0812</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -44,7 +50,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Kaligrafi'])[1]/preceding::a[2]</value>
-      <webElementGuid>b090e235-15c0-44f0-a82a-4c748588e15c</webElementGuid>
+      <webElementGuid>56162053-c709-426b-b4ae-504f3feb74df</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -52,7 +58,7 @@
       <name>xpath:href</name>
       <type>Main</type>
       <value>//a[contains(@href, '/?category_id=4')]</value>
-      <webElementGuid>10d077ba-d4d4-4fe0-9fa2-cabc9b7e1735</webElementGuid>
+      <webElementGuid>5d214f87-ff29-49f2-8826-e51158c5274c</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -60,7 +66,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//a[5]</value>
-      <webElementGuid>6628f2c8-4285-4891-91b8-95534730dc1f</webElementGuid>
+      <webElementGuid>948b26a9-cf72-4ec9-8a35-33f4f7146191</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -74,6 +80,6 @@
         
         Elektronik
 ')]</value>
-      <webElementGuid>b6f2b64d-c6b0-4256-8faf-3c7d41e5dfd8</webElementGuid>
+      <webElementGuid>b0a458fd-cab5-4215-be33-b8f065edd370</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
